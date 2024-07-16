d1c2fc82b65e9597b3f86e8af3485b85e87860a1
Kirill Vercetti <office@kyzima-spb.com>
2024-07-16 14:40:56 +0300

    Рефакторинг

--- 11-docker-compose/uploader/src/frontend/vite.config.js
+++ 11-docker-compose/uploader/src/frontend/vite.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const proxyDefaults = {
+  target: 'http://backend:5000',
+  changeOrigin: true,
+}
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://backend:5000',
-        changeOrigin: true,
+        ...proxyDefaults,
         rewrite: path => path.replace(/^\/api/, ''),
       },
-      '/media': {
-        target: 'http://backend:5000',
-        changeOrigin: true,
-      },
+      '/media': proxyDefaults,
     },
   },
 })
+

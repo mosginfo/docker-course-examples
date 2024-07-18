a61dfb18c2dd07edcbecf07c3df18b956e7c8217
Kirill Vercetti <office@kyzima-spb.com>
2024-07-18 17:36:33 +0300

    Обновочка

--- 12-secrets/compose-secrets/bin/build.sh
+++ 12-secrets/compose-secrets/bin/build.sh
@@ -7,13 +7,23 @@ MONGO_ROOT_USERNAME="$SECRET_DIR/mongo_root_username"
 MONGO_ROOT_PASSWORD="$SECRET_DIR/mongo_root_password"
 
 if [ ! -f "$MONGO_ROOT_USERNAME" ]; then
-  echo "Creating $(basename $MONGO_ROOT_USERNAME) secret"
-  echo 'user' > "$MONGO_ROOT_USERNAME"
+  username=''
+
+  while [ -z "$username" ]; do
+      read -p 'Enter Mongo superuser name: ' username
+  done
+
+  echo -n "$username" > "$MONGO_ROOT_USERNAME"
 fi
 
 if [ ! -f "$MONGO_ROOT_PASSWORD" ]; then
-  echo "Creating $(basename $MONGO_ROOT_PASSWORD) secret"
-  echo "$(command -v pwgen >/dev/null 2>&1 && pwgen -cn 16 1 || openssl rand -base64 32)" > "$MONGO_ROOT_PASSWORD"
+  read -p 'Enter Mongo superuser password: ' password
+
+  if [ -z "$password" ]; then
+      password="$(command -v pwgen >/dev/null 2>&1 && pwgen -cn 16 1 || openssl rand -base64 32)"
+  fi
+
+  echo -n "$password" > "$MONGO_ROOT_PASSWORD"
 fi
 
 docker compose build
